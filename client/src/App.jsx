26cb30d82ce71f052f466c36e30e2d52a5b9f38d
test <test@gmail.com>
2025-08-17 16:34:06 +0530

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,11 @@ import LandingPage from './components/LandingPage'
 import LoginPage from './components/LoginPage'
 import PlaylistPage from './components/PlaylistPage'
 import PredictionPage from './components/PredictionsPage'
+import ProfilePage from './components/ProfilePage'
 import RegisterPage from './components/RegisterPage'
 import ThoughtsPage from './components/ThoughtsPage'
 import VideoPage from './components/VideoPage'
+import Routes from './Routes/Routes'
 
 function App() {
   return (
@@ -23,6 +25,8 @@ function App() {
       <AudioPage/>
       <PlaylistPage/>
       <PredictionPage/>
+      <ProfilePage/>
+   
     </>
   )
 }
